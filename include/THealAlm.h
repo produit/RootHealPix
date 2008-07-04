0fd4aa99fbd1d7c158f551a8e68b03490f7a20e8
oxon <oxon>
2008-07-04 22:11:33 +0000

    Add operators

--- include/THealAlm.h
+++ include/THealAlm.h
@@ -1,4 +1,4 @@
-// $Id: THealAlm.h,v 1.2 2008/06/27 18:35:55 oxon Exp $
+// $Id: THealAlm.h,v 1.3 2008/07/04 22:11:33 oxon Exp $
 // Author: Akira Okumura 2008/06/26
 
 /*****************************************************************************
@@ -17,15 +17,22 @@
 #include "THealFFT.h"
 
 class THealPix;
+template<typename T> class THealAlm;
+template<typename T> THealAlm<T> operator*(Double_t c1, const THealAlm<T>& ha1);
+template<typename T> THealAlm<T> operator*(const THealAlm<T>& ha1, Double_t c1);
+template<typename T> THealAlm<T> operator+(const THealAlm<T>& ha1, const THealAlm<T>& ha2);
+template<typename T> THealAlm<T> operator-(const THealAlm<T>& ha1, const THealAlm<T>& ha2);
+template<typename T> THealAlm<T> operator*(const THealAlm<T>& ha1, const THealAlm<T>& ha2);
+template<typename T> THealAlm<T> operator/(const THealAlm<T>& ha1, const THealAlm<T>& ha2);
 
 template<typename T> class THealAlm : public TObject {
 private:
   std::vector<std::complex<T> > fAlm;
-  std::complex<T> fAlmDummy;
-  static const std::complex<T> fAlmDummyConst;
-  Int_t fLmax;
-  Int_t fMmax;
-  Int_t fTval;
+  static std::complex<T> fgAlmDummy; //!
+  static const std::complex<T> fgAlmDummyConst; //!
+  Int_t fLmax; // Maximum of l
+  Int_t fMmax; // Maximum of m
+  Int_t fTval; //!to be used for faster calculation
 
 protected:
   virtual void Copy(TObject& hanew) const;
@@ -35,24 +42,27 @@ public:
   THealAlm(const THealAlm<T>& ha);
   virtual ~THealAlm();
   virtual void  Add(const THealAlm<T>* ha1, Double_t c1 = 1);
+  virtual void  Add(const THealAlm<T>* ha1, const THealAlm<T>* ha2, Double_t c1, Double_t c2);
   virtual Int_t GetLmax() const {return fLmax;}
   virtual Int_t GetMmax() const {return fMmax;}
   virtual std::complex<T>* GetMstart(Int_t m) {return &(fAlm[(m*(fTval - m))>>1]);}
   virtual void  Divide(const THealAlm<T>* ha1);
   virtual void  Multiply(const THealAlm<T>* ha1);
   virtual void  ResetLM(Int_t lmax, Int_t mmax);
+  virtual void  Scale(Double_t c1);
+  virtual void  SetAlm(Int_t l, Int_t m, std::complex<T> a) {(*this)(l, m) = a;}
   virtual void  SetToZero();
 
         std::complex<T>& operator()(Int_t l, Int_t m);
   const std::complex<T>& operator()(Int_t l, Int_t m) const;
 
-          THealAlm<T>& operator=(const THealAlm<T>& ha1);
-  friend  THealAlm<T>  operator*(Double_t c1, const THealAlm<T>& ha1);
-  friend  THealAlm<T>  operator*(const THealAlm<T>& ha1, Double_t c1);
-  friend  THealAlm<T>  operator+(const THealAlm<T>& ha1, const THealAlm<T>& ha2);
-  friend  THealAlm<T>  operator-(const THealAlm<T>& ha1, const THealAlm<T>& ha2);
-  friend  THealAlm<T>  operator*(const THealAlm<T>& ha1, const THealAlm<T>& ha2);
-  friend  THealAlm<T>  operator/(const THealAlm<T>& ha1, const THealAlm<T>& ha2);
+  THealAlm<T>& operator=(const THealAlm<T>& ha1);
+  friend THealAlm<T>  operator*<>(Double_t c1, const THealAlm<T>& ha1);
+  friend THealAlm<T>  operator*<>(const THealAlm<T>& ha1, Double_t c1);
+  friend THealAlm<T>  operator+<>(const THealAlm<T>& ha1, const THealAlm<T>& ha2);
+  friend THealAlm<T>  operator-<>(const THealAlm<T>& ha1, const THealAlm<T>& ha2);
+  friend THealAlm<T>  operator*<>(const THealAlm<T>& ha1, const THealAlm<T>& ha2);
+  friend THealAlm<T>  operator/<>(const THealAlm<T>& ha1, const THealAlm<T>& ha2);
 
   // Operators for PyROOT
   virtual THealAlm<T>  __add__(const THealAlm<T>& ha1) const;
@@ -62,17 +72,12 @@ public:
   virtual THealAlm<T>  __rmul__(Double_t c1) const;
   virtual THealAlm<T>  __sub__(const THealAlm<T>& ha1) const;
 
-  ClassDef(THealAlm, 0);
+  ClassDef(THealAlm, 1);
 };
 
-template<typename T> THealAlm<T> operator*(Double_t c1, const THealAlm<T>& ha1);
 template<typename T> inline THealAlm<T> operator*(const THealAlm<T>& ha1, Double_t c1)
 {
   return operator*(c1, ha1);
 }
-template<typename T> THealAlm<T> operator+(const THealAlm<T>& ha1, const THealAlm<T>& ha2);
-template<typename T> THealAlm<T> operator-(const THealAlm<T>& ha1, const THealAlm<T>& ha2);
-template<typename T> THealAlm<T> operator*(const THealAlm<T>& ha1, const THealAlm<T>& ha2);
-template<typename T> THealAlm<T> operator/(const THealAlm<T>& ha1, const THealAlm<T>& ha2);
 
 #endif // T_HEAL_ALM
--- src/THealAlm.cxx
+++ src/THealAlm.cxx
@@ -1,4 +1,4 @@
-// $Id: THealAlm.cxx,v 1.4 2008/07/03 22:31:45 oxon Exp $
+// $Id: THealAlm.cxx,v 1.5 2008/07/04 22:11:33 oxon Exp $
 // Author: Akira Okumura 2008/06/26
 
 /*****************************************************************************
@@ -10,12 +10,13 @@
 
 #include "THealAlm.h"
 #include "THealPix.h"
-#include "THealUtil.h"
 
 templateClassImp(THealAlm)
 
 template<typename T>
-const std::complex<T> THealAlm<T>::fAlmDummyConst = std::complex<T>(0, 0);
+std::complex<T> THealAlm<T>::fgAlmDummy = std::complex<T>(0, 0);
+template<typename T>
+const std::complex<T> THealAlm<T>::fgAlmDummyConst = std::complex<T>(0, 0);
 
 //_____________________________________________________________________________
 template<typename T>
@@ -43,9 +44,48 @@ template<typename T>
 void THealAlm<T>::Add(const THealAlm<T>* ha1, Double_t c1)
 {
   if(!ha1){
-    Error("Divide", "Attempt to divide by a non-existing Alm");
+    Error("Add", "Attempt to add a non-existing Alm");
     return;
   } // if
+
+  if(fLmax != ha1->GetLmax() || fMmax != ha1->GetMmax()){
+    Error("Add", "Attempt to add Alms with different number of lmax or mmax");
+    return;
+  } // if
+
+  for(Int_t l = 0; l <= fLmax; l++){
+    for(Int_t m = 0; m <= TMath::Min(l, fMmax); m++){
+      std::complex<T> comp = ((*ha1)(l, m));
+      comp *= c1;
+      (*this)(l, m) += comp;
+    } // m
+  } // l
+}
+
+//______________________________________________________________________________
+template<typename T>
+void THealAlm<T>::Add(const THealAlm<T>* ha1, const THealAlm<T>* ha2, Double_t c1, Double_t c2)
+{
+  if(!ha1 || !ha2){
+    Error("Add", "Attempt to add a non-existing Alm");
+    return;
+  } // if
+
+  if(fLmax != ha1->GetLmax() || fMmax != ha1->GetMmax()
+  || fLmax != ha2->GetLmax() || fMmax != ha2->GetMmax()){
+    Error("Add", "Attempt to add Alms with different number of lmax or mmax");
+    return;
+  } // if
+
+  for(Int_t l = 0; l <= fLmax; l++){
+    for(Int_t m = 0; m <= TMath::Min(l, fMmax); m++){
+      std::complex<T> comp1 = ((*ha1)(l, m));
+      std::complex<T> comp2 = ((*ha2)(l, m));
+      comp1 *= c1;
+      comp2 *= c2;
+      (*this)(l, m) = comp1 + comp2;
+    } // m
+  } // l
 }
 
 //_____________________________________________________________________________
@@ -67,6 +107,21 @@ void THealAlm<T>::Divide(const THealAlm<T>* ha1)
     Error("Divide", "Attempt to divide by a non-existing Alm");
     return;
   } // if
+
+  if(fLmax != ha1->GetLmax() || fMmax != ha1->GetMmax()){
+    Error("Divide", "Attempt to devide Alms with different number of lmax or mmax");
+    return;
+  } // if
+
+  for(Int_t l = 0; l <= fLmax; l++){
+    for(Int_t m = 0; m <= TMath::Min(l, fMmax); m++){
+      if(std::norm((*ha1)(l, m)) != 0){
+	(*this)(l, m) /= (*ha1)(l, m);
+      } else {
+	(*this)(l, m) = 0;
+      } // if
+    } // m
+  } // l
 }
 
 //_____________________________________________________________________________
@@ -77,6 +132,17 @@ void THealAlm<T>::Multiply(const THealAlm<T>* ha1)
     Error("Multiply", "Attempt to multiply a non-existing Alm");
     return;
   } // if
+
+  if(fLmax != ha1->GetLmax() || fMmax != ha1->GetMmax()){
+    Error("Divide", "Attempt to multiply Alms with different number of lmax or mmax");
+    return;
+  } // if
+
+  for(Int_t l = 0; l <= fLmax; l++){
+    for(Int_t m = 0; m <= TMath::Min(l, fMmax); m++){
+      (*this)(l, m) *= (*ha1)(l, m);
+    } // m
+  } // l
 }
 
 //_____________________________________________________________________________
@@ -89,7 +155,14 @@ void THealAlm<T>::ResetLM(Int_t lmax, Int_t mmax)
   fTval = 2*fLmax + 1;
   Int_t n = (fMmax + 1)*(fMmax + 2)/2 + (fMmax + 1)*(fLmax - fMmax);
 
-  fAlm.resize(n, fAlmDummyConst);
+  fAlm.resize(n, fgAlmDummyConst);
+}
+
+//_____________________________________________________________________________
+template<typename T>
+void THealAlm<T>::Scale(Double_t c1)
+{
+  Add(this, this, c1, 0);
 }
 
 //_____________________________________________________________________________
@@ -97,31 +170,45 @@ template<typename T>
 void THealAlm<T>::SetToZero()
 {
   for(UInt_t i = 0; i < fAlm.size(); i++){
-    fAlm[i] = fAlmDummyConst;
+    fAlm[i] = fgAlmDummyConst;
   } // i
+}
+
+//______________________________________________________________________________
+template<typename T>
+void THealAlm<T>::Streamer(TBuffer& b)
+{
+  if(b.IsReading()){
+    UInt_t R__s, R__c;
+    Version_t R__v = b.ReadVersion(&R__s, &R__c);
+    b.ReadClassBuffer(THealPix::Class(), this, R__v, R__s, R__c);
+    fTval = 2*fLmax + 1;
+  } else {
+    b.WriteClassBuffer(THealPix::Class(), this);
+  }
 }
 
 //______________________________________________________________________________
 template<typename T>
 std::complex<T>& THealAlm<T>::operator()(Int_t l, Int_t m)
 {
-  if(0 <= l && l <= fLmax && 0 <= m && m <= l){
+  if(0 <= l && l <= fLmax && 0 <= m && m <= TMath::Min(fMmax, l)){
     return fAlm[((m*(fTval - m))>>1) + l];
   } // if
 
-  fAlmDummy = fAlmDummyConst;
-  return fAlmDummy;
+  fgAlmDummy = fgAlmDummyConst;
+  return fgAlmDummy;
 }
 
 //______________________________________________________________________________
 template<typename T>
 const std::complex<T>& THealAlm<T>::operator()(Int_t l, Int_t m) const
 {
-  if(0 <= l && l <= fLmax && 0 <= m && m <= l){
+  if(0 <= l && l <= fLmax && 0 <= m && m <= TMath::Min(fMmax, l)){
     return fAlm[((m*(fTval - m))>>1) + l];
   } // if
 
-  return fAlmDummyConst;
+  return fgAlmDummyConst;
 }
 
 //______________________________________________________________________________
@@ -189,7 +276,6 @@ template<typename T>
 THealAlm<T> operator*(Double_t c1, const THealAlm<T>& ha1)
 {
    // Operator *
-
    THealAlm<T> hanew = ha1;
    hanew.Scale(c1);
    return hanew;
@@ -200,7 +286,6 @@ template<typename T>
 THealAlm<T> operator+(const THealAlm<T>& ha1, const THealAlm<T>& ha2)
 {
    // Operator +
-
    THealAlm<T> hanew = ha1;
    hanew.Add(&ha2, 1);
    return hanew;
@@ -211,7 +296,6 @@ template<typename T>
 THealAlm<T> operator-(const THealAlm<T>& ha1, const THealAlm<T>& ha2)
 {
    // Operator -
-
    THealAlm<T> hanew = ha1;
    hanew.Add(&ha2, -1);
    return hanew;
@@ -222,7 +306,6 @@ template<typename T>
 THealAlm<T> operator*(const THealAlm<T>& ha1, const THealAlm<T>& ha2)
 {
    // Operator *
-
    THealAlm<T> hanew = ha1;
    hanew.Multiply(&ha2);
    return hanew;
@@ -233,7 +316,6 @@ template<typename T>
 THealAlm<T> operator/(const THealAlm<T>& ha1, const THealAlm<T>& ha2)
 {
    // Operator /
-
    THealAlm<T> hanew = ha1;
    hanew.Divide(&ha2);
    return hanew;
