45a911ba6ba8ac96f9adcc5f8a982109a1ae63ce
oxon <oxon>
2008-07-07 07:00:13 +0000

    Merge THealAlmMapTools with THealUtil

--- include/LinkDef.h
+++ include/LinkDef.h
@@ -1,4 +1,4 @@
-// $Id: LinkDef.h,v 1.11 2008/07/04 22:10:19 oxon Exp $
+// $Id: LinkDef.h,v 1.12 2008/07/07 07:04:03 oxon Exp $
 // Author: Akira Okumura 2008/06/20
 
 /*****************************************************************************
@@ -47,7 +47,7 @@
 #pragma link C++ function operator*(THealPixD&, THealPixD&);
 #pragma link C++ function operator/(THealPixD&, THealPixD&);
 
-namespace THealAlmMapTools {
+namespace THealUtil {
   // Work around a CINT instantiation problem ...
   void Map2Alm(const THealPix& map, THealAlm<Float_t>& alm, Bool_t add_alm = kFALSE);
   void Map2Alm(const THealPix& map, THealAlm<Double_t>& alm, Bool_t add_alm = kFALSE);
@@ -56,15 +56,14 @@ namespace THealAlmMapTools {
   void Map2Alm(const THealPix& map, THealAlm<Double_t>& alm, const
 	       std::vector<Double_t>& weight, Bool_t add_alm = kFALSE);
 }
-#pragma link C++ namespace THealAlmMapTools;
-#pragma link C++ function THealAlmMapTools::Alm2Map(const THealAlm<Float_t>&, THealPix&);
-#pragma link C++ function THealAlmMapTools::Alm2Map(const THealAlm<Double_t>&, THealPix&);
-#pragma link C++ function THealAlmMapTools::Map2Alm(const THealPix&, THealAlm<Float_t>&, Bool_t);
-#pragma link C++ function THealAlmMapTools::Map2Alm(const THealPix&, THealAlm<Double_t>&, Bool_t);
-#pragma link C++ function THealAlmMapTools::Map2Alm(const THealPix&, THealAlm<Float_t>&, std::vector<Double_t>&, Bool_t);
-#pragma link C++ function THealAlmMapTools::Map2Alm(const THealPix&, THealAlm<Double_t>&, std::vector<Double_t>&, Bool_t);
-
 #pragma link C++ namespace THealUtil;
+#pragma link C++ function THealUtil::Alm2Map(const THealAlm<Float_t>&, THealPix&);
+#pragma link C++ function THealUtil::Alm2Map(const THealAlm<Double_t>&, THealPix&);
+#pragma link C++ function THealUtil::Map2Alm(const THealPix&, THealAlm<Float_t>&, Bool_t);
+#pragma link C++ function THealUtil::Map2Alm(const THealPix&, THealAlm<Double_t>&, Bool_t);
+#pragma link C++ function THealUtil::Map2Alm(const THealPix&, THealAlm<Float_t>&, std::vector<Double_t>&, Bool_t);
+#pragma link C++ function THealUtil::Map2Alm(const THealPix&, THealAlm<Double_t>&, std::vector<Double_t>&, Bool_t);
+
 #pragma link C++ function THealUtil::FitsReportError(Int_t);
 #pragma link C++ function THealUtil::Nside2Npix(Int_t);
 #pragma link C++ function THealUtil::Order2Nside(Int_t);
--- include/THealUtil.h
+++ include/THealUtil.h
@@ -1,4 +1,4 @@
-// $Id: THealUtil.h,v 1.4 2008/07/01 23:03:35 oxon Exp $
+// $Id: THealUtil.h,v 1.5 2008/07/07 07:00:13 oxon Exp $
 // Author: Akira Okumura 2008/06/20
 
 /*****************************************************************************
@@ -15,13 +15,19 @@
 #include "Rtypes.h"
 #include "TMath.h"
 
+template<typename T> class THealAlm;
+class THealPix;
+
 namespace THealUtil {
+  template<typename T> void Alm2Map(const THealAlm<T>& alm, THealPix& map);
+  template<typename T> void Map2Alm(const THealPix& map, THealAlm<T>& alm, Bool_t add_alm = kFALSE);
+  template<typename T> void Map2Alm(const THealPix& map, THealAlm<T>& alm, const std::vector<double>& weight, Bool_t add_alm = kFALSE);
+
   inline Int_t    Nside2Npix(Int_t nside) { return 12*nside*nside;}
   inline Int_t    Order2Nside(Int_t order) { return 1<<order;}
 
   Bool_t          FitsReportError(Int_t status);
   inline UInt_t   Isqrt(UInt_t v);
-  void            GetChunkInfo(Int_t nrings, Int_t& nchunks, Int_t& chunksize);
   inline Double_t Modulo(Double_t v1, Double_t v2);
   inline Int_t    Modulo(Int_t v1, Int_t v2);
   inline Long_t   Modulo(Long_t v1, Long_t v2);
@@ -30,30 +36,34 @@ namespace THealUtil {
   Bool_t          SaveToFits(const char* fname, const std::vector<const THealPix*>& hp);
 };
 
-//_____________________________________________________________________________
+//______________________________________________________________________________
 UInt_t THealUtil::Isqrt(UInt_t v)
 {
+  // Calculate square root of an integer
   // Original code is isqrt of HEALPix C++
   return UInt_t(TMath::Sqrt(v + .5));
 }
 
-//_____________________________________________________________________________
+//______________________________________________________________________________
 Double_t THealUtil::Modulo(Double_t v1, Double_t v2)
 {
+  // Calculate modulo of two doubles
   // Original code is modulo of HEALPix C++
   return (v1 >= 0) ? ((v1 < v2) ? v1 : fmod(v1, v2)) : (fmod(v1, v2) + v2);
 }
 
-//_____________________________________________________________________________
+//______________________________________________________________________________
 Int_t THealUtil::Modulo(Int_t v1, Int_t v2)
 {
+  // Calculate modulo of two integers
   // Original code is modulo of HEALPix C++
   return (v1 >= 0) ? ((v1 < v2) ? v1 : (v1%v2)) : ((v1%v2) + v2);
 }
 
-//_____________________________________________________________________________
+//______________________________________________________________________________
 Long_t THealUtil::Modulo(Long_t v1, Long_t v2)
 {
+  // Calculate modulo of two long integers
   // Original code is modulo of HEALPix C++
   return (v1 >= 0) ? ((v1 < v2) ? v1 : (v1%v2)) : ((v1%v2) + v2);
 }
